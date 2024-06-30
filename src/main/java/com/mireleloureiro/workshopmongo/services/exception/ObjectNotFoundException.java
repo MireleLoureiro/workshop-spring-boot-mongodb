bbc9c1fa3f545a7d70746489af8eec0c7426aa11
Mirele Loureiro <miloureiro112@gmail.com>
2024-06-30 14:47:33 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/mireleloureiro/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mireleloureiro.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
