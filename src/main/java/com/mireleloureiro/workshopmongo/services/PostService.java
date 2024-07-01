0ccb78b7d3409c2e1575f2d85151db43ae7856b1
Mirele Loureiro <miloureiro112@gmail.com>
2024-06-30 21:31:47 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mireleloureiro/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.mireleloureiro.workshopmongo.services;
+
+import com.mireleloureiro.workshopmongo.domain.Post;
+import com.mireleloureiro.workshopmongo.repository.PostRepository;
+import com.mireleloureiro.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
